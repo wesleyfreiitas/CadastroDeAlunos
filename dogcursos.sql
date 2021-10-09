17c5484ee6129a0ded4b6f18fc76ff0dfb2040dd
wesleyfreiitas <wesley8bu@gmail.com>
2021-10-08 21:26:08 -0300

    :x

---
+++ dogcursos.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09-Out-2021 às 01:59
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `dogcursos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `id_aluno` int(11) NOT NULL,
+  `nome_aluno` varchar(45) DEFAULT NULL,
+  `data_nascimento` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id_aluno`, `nome_aluno`, `data_nascimento`) VALUES
+(2, 'joao', '03-12-1998');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos_cursos`
+--
+
+CREATE TABLE `alunos_cursos` (
+  `id_aluno_curso` int(11) NOT NULL,
+  `id_curso` int(11) DEFAULT NULL,
+  `id_aluno` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `alunos_cursos`
+--
+
+INSERT INTO `alunos_cursos` (`id_aluno_curso`, `id_curso`, `id_aluno`) VALUES
+(0, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id_curso` int(11) NOT NULL,
+  `nome_curso` varchar(45) DEFAULT NULL,
+  `carga_horaria` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cursos`
+--
+
+INSERT INTO `cursos` (`id_curso`, `nome_curso`, `carga_horaria`) VALUES
+(0, 'Enfermagem', '80'),
+(2, 'computação cienctifica', '80');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `senha` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `senha`) VALUES
+(1, 'admin', 'admin');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id_aluno`);
+
+--
+-- Índices para tabela `alunos_cursos`
+--
+ALTER TABLE `alunos_cursos`
+  ADD PRIMARY KEY (`id_aluno_curso`),
+  ADD KEY `id_aluno` (`id_aluno`),
+  ADD KEY `id_curso` (`id_curso`);
+
+--
+-- Índices para tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id_curso`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `alunos_cursos`
+--
+ALTER TABLE `alunos_cursos`
+  ADD CONSTRAINT `alunos_cursos_ibfk_1` FOREIGN KEY (`id_aluno`) REFERENCES `alunos` (`id_aluno`),
+  ADD CONSTRAINT `alunos_cursos_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
